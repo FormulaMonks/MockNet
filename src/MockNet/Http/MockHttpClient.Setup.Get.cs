c5039737612d0ea07a7ed4014d5dfd130dd20d0b
Devin Sackett <dsac@hotmail.com>
2019-12-02 06:36:46 -0800

    Document the public Setup APIs

--- src/MockNet/Http/MockHttpClient.Setup.Get.cs
+++ src/MockNet/Http/MockHttpClient.Setup.Get.cs
@@ -5,11 +5,20 @@ namespace MockNet.Http
 {
     public partial class MockHttpClient
     {
+        /// <summary>
+        /// Specifices a setup on the HTTP GET protocol.
+        /// </summary>
+        /// <param name="uri">The uri to match the setup with.</param>
         public ISetup SetupGet(string uri)
         {
             return Setup(HttpMethod.Get, uri);
         }
 
+        /// <summary>
+        /// Specifies a setup on the HTTP GET protocol.
+        /// </summary>
+        /// <param name="uri">The uri to match the setup with.</param>
+        /// <param name="headers">Lambda predicate that specifics the match on headers.</param>
         public ISetup SetupGet(string uri, Expression<Func<HttpRequestHeaders, bool>> headers)
         {
             return Setup(HttpMethod.Get, uri, headers);
--- src/MockNet/Http/MockHttpClient.Setup.Post.cs
+++ src/MockNet/Http/MockHttpClient.Setup.Post.cs
@@ -5,16 +5,31 @@ namespace MockNet.Http
 {
     public partial class MockHttpClient
     {
+        /// <summary>
+        /// Specifices a setup on the HTTP POST protocol.
+        /// </summary>
+        /// <param name="uri">The uri to match the setup with.</param>
         public ISetup SetupPost(string uri)
         {
             return Setup(HttpMethod.Post, uri);
         }
 
+        /// <summary>
+        /// Specifies a setup on the HTTP POST protocol.
+        /// </summary>
+        /// <param name="uri">The uri to match the setup with.</param>
+        /// <param name="headers">Lambda predicate that specifics the match on headers.</param>
         public ISetup SetupPost(string uri, Expression<Func<HttpRequestHeaders, bool>> headers)
         {
             return Setup(HttpMethod.Post, uri, headers);
         }
 
+        /// <summary>
+        /// Specifies a setup on the HTTP POST protocol.
+        /// </summary>
+        /// <param name="uri">The uri to match the setup with.</param>
+        /// <param name="headers">Lambda predicate that specifics the match on headers.</param>
+        /// <param name="content">Lambda predicate that specifies the match on content.</param>
         public ISetup SetupPost<TBody>(string uri, Expression<Func<HttpRequestHeaders, bool>> headers, Expression<Func<TBody, bool>> content)
         {
             return Setup(HttpMethod.Post, uri, headers, content);
