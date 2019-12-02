ea1ca5eef40185d6436b8702203b0e213583bda4
Devin Sackett <dsac@hotmail.com>
2019-12-02 06:41:37 -0800

    Throw exception for non-implemented code

--- src/MockNet/Http/Contents/MultipartContent.cs
+++ src/MockNet/Http/Contents/MultipartContent.cs
@@ -1,9 +1,10 @@
+using System;
 using SystemHttpContent = System.Net.Http.HttpContent;
 
 namespace MockNet.Http
 {
     public class MultipartContent : IHttpContent
     {
-        public SystemHttpContent ToHttpContent() => null;
+        public SystemHttpContent ToHttpContent() => throw new NotImplementedException();
     }
 }
